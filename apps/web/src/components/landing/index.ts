c0e8139b204f4798284ee549c3229705b1ed6825
yash <yashgouravkar@gmail.com>
2026-05-24 07:00:13 +0530

    fix

--- apps/web/src/components/landing/index.ts
+++ apps/web/src/components/landing/index.ts
@@ -1,4 +1,3 @@
-export { FadeInOnScroll } from "./fade-in-on-scroll";
 export { FrostButton } from "./frost-button";
 export { HeroAtmosphere } from "./hero-atmosphere";
 export { TopAnnouncement } from "./top-announcement";
